96f1bc338a7bd9aff8cd3191123b5428c59cfa79
Kristian <kristian.panku@gmail.com>
2021-08-28 21:22:13 -0600

    finished numbers

---
+++ number.py
@@ -0,0 +1,24 @@
+# Integers, no decimal
+print(3)
+print(int())
+print(int(3))
+print(int(3.0))  # Type casted (conversion from one type to another)
+
+# Floating Point Number, has decimal
+print(4.0)
+print(float())
+print(float(4.0))
+print(float(4))  # Type casted (conversion from one type to another)
+
+# Type casting to string
+print(str(7.0) + ' and ' + str(17))
+
+# print(7.0 + ' and ' + 7) Will result in TypeError
+
+print(1+1)
+print(2-1)
+print(2*2)
+print(4/2)  # Division always results in a float
+print(10 % 3)  # Modulo returns the remaining number
+print(10//3)  # Integer Division returns the amount of times a number fits in divison
+print(2**4)  # Exponent multiplies number 1 by itself number 2 amount of times
